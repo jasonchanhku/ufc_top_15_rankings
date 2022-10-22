b840a3fc232c56f273eb0e82c367b675c506b154
Jason Chan <jasonchanhku@gmail.com>
2022-10-22 18:32:27 +0800

    completed scraper

--- scraper.py
+++ scraper.py
@@ -1,20 +1,44 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
+import requests
+from bs4 import BeautifulSoup
 import pandas as pd
 import sqlite3
 
+rankingsLink = 'https://www.ufc.com/rankings'
 
-# Read sqlite query results into a pandas DataFrame
+data = requests.get(rankingsLink)
+soup = BeautifulSoup(data.text, 'html.parser')
+
+
+segments = soup.find_all('div', {'class': 'view-grouping-content'}, href=False)   
+
+excludeList = ["Men's Pound-for-Pound Top Rank", "Women's Pound-for-Pound Top Rank", "Women's Featherweight"]
+dfList = []
+for segment in segments:
+    weightclass = segment.find('div', {'class': 'info'}).find('h4').text.strip()
+    if weightclass in excludeList:
+        continue
+    subDf = pd.DataFrame()
+    print(f"{weightclass} Top 15")
+    fighters = segment.find_all('div', {'class':'views-row'})
+    fighters = [fighter.text.strip() for fighter in fighters]
+    print(fighters)
+    print("==========\n")
+    subDf['fighters'] = fighters
+    subDf['weightclass'] = weightclass
+    subDf['rank'] = [r for r in range(1, len(fighters)+1)]
+    subDf = subDf[['weightclass','rank', 'fighters']]
+    dfList.append(subDf)
+    
+        
+
+finalDf = pd.concat(dfList)
+
+print(finalDf.head())
+
 conn = sqlite3.connect('data.sqlite')
-df = pd.read_sql_query("SELECT * from data", conn)
-
-# Verify that result of SQL query is stored in the dataframe
-print(df.head())
-
-
-df = pd.DataFrame([['c', 3, 'cat'], ['d', 4, 'dog']],
-                   columns=['letter', 'number', 'animal'])
-
-#conn = sqlite3.connect('data.sqlite')
-df.to_sql('data', conn, if_exists='append')
+finalDf.to_sql('data', conn, if_exists='replace')
+print('UFC Fighters Rankings successfully scraped and updated')
+conn.close()
